ca2c0fee1c6c242d64014a38c28414e8e3a6a9c0
Snopinio <jansnopkowski64@gmail.com>
2023-03-31 10:52:32 +0200

    CALE SKONCZONE SZYFROWANIE Z WPROWADZANIEM

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -33,9 +33,72 @@
     return cypherText;
 }
 
+string gaderypoluki2(string clearText)
+{
+    string cypherText = "";
+
+    foreach (char c in clearText)
+    {
+        //wyonaj ponizsze instrucje dla kazdej itery w tekscie zrodloym
+        // c = jedna literka z tekstu
+        switch(c)
+        {
+            case 'G':
+                cypherText += "A";
+                break;
+            case 'A':
+                cypherText += "G";
+                break;
+            case 'D':
+                cypherText += "E";
+                break;
+            case 'E':
+                cypherText += "D";
+                break;
+            case 'R':
+                cypherText += "Y";
+                break;
+            case 'Y':
+                cypherText += "R";
+                break;
+            case 'P':
+                cypherText += "O";
+                break;
+            case 'O':
+                cypherText += "P";
+                break;
+            case 'L':
+                cypherText += "U";
+                break;
+            case 'U':
+                cypherText += "L";
+                break;
+            case 'K':
+                cypherText += "I";
+                break;
+            case 'I':
+                cypherText += "K";
+                break;
+            default:
+                cypherText += c; 
+                break;
+
+        }
+    }
+    return cypherText;
+}
+
 //tekst do zaszyfrowania
-string clearText = "GADERYPOLUKI";
+string clearText = "";
+Console.WriteLine("Prosze podaj tekst do zaszyfrowania: ");
+clearText= Console.ReadLine() ?? "";
+
+//zamien na wielkie litery
+clearText= clearText.ToUpper();
+
 //zaszyfruj uzywajac funkcji
 string cypherText = gaderypuluki(clearText);
 
-Console.WriteLine("Zaszyfrowany tekst: " + cypherText);
+Console.WriteLine("Zaszyfrowany tekst (metoda1):  " + cypherText);
+Console.WriteLine("Zaszyfrowany tekst (metoda2):  " + cypherText);
+
